e0e214a2ff024fc4a479f6d14eb06dc9207ed65d
Chris Heinen <cheinen@arn.com>
2014-11-19 15:07:43 -0500

    added startup overlay

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 var arnieApp = angular.module('arnieApp',[]);
 
-arnieApp.controller('ArnieController', ['$scope', '$http', function($scope, $http) {
-	// TODO: should these be individual or just tied to the status obj?
+arnieApp.controller('ArnieController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+  // TODO: should these be individual or just tied to the status obj?
+  $scope.status = {}; // TODO: use this instead of the below vars
+  $scope.overlay = true; // Start with overlay
   $scope.alreadyConnected = false;
   $scope.sessionStarted = false;
   $scope.vendInProgress = false;
@@ -12,16 +14,21 @@ arnieApp.controller('ArnieController', ['$scope', '$http', function($scope, $htt
       seconds;
 
   // When the app starts, connect to PC2MDB
-  if(!$scope.alreadyConnected){
-    $http.get('/connect').success(function(data){
-      console.log("connected to machine", data);
-      $scope.alreadyConnected = true;
-    });
-  }
+  // if(!$scope.alreadyConnected){
+  //   $http.get('/connect').success(function(data){
+  //     console.log("connected to machine", data);
+  //     $scope.status = data;
+  //     // TODO: replace alreadyConnect with machineReady?
+  //     $scope.alreadyConnected = true;
+  //     $timeout(function(){
+  //       $scope.overlay = false; // Hide startup overlay
+  //     },1500);
+  //   });
+  // }
 
   // Start vending session
   $scope.startSession = function(){
-  	// TODO: page should be disabled untile machineReady is true
+    // TODO: page should be disabled untile machineReady is true
     $http.get('/status').success(function(data){
       console.log('startsession status',data);
     });
@@ -38,52 +45,51 @@ arnieApp.controller('ArnieController', ['$scope', '$http', function($scope, $htt
   $scope.endSession = function(){
     // First checkstatus to see if vend is taking place
     $http.get('/status').success(function(data){
-    	console.log('endsession status',data);
-		$scope.vendInProgress = data.vendInProgress;
-  		// If a vend is already in progress, prevent cancel
-  		if (!$scope.vendInProgress) {
-  			// Request to end session
-      		$http.get('/requestendsession').success(function(data){
-        		clearTimeout(timer);
-        		console.log("requested end session", data);
-        		// Actually end the session
-        		$http.get('/endsession').success(function(){
-        			console.log("ended session", data);
-        			// Reset scope vars
-        			$scope.vendInProgress = data.vendInProgress;
-        			$scope.sessionStarted = data.sessionStarted;
-        		});
-      		});
-      	}
+      console.log('endsession status',data);
+    $scope.vendInProgress = data.vendInProgress;
+      // If a vend is already in progress, prevent cancel
+      if (!$scope.vendInProgress) {
+        // Request to end session
+          $http.get('/requestendsession').success(function(data){
+            clearTimeout(timer);
+            console.log("requested end session", data);
+            // Actually end the session
+            $http.get('/endsession').success(function(){
+              console.log("ended session", data);
+              // Reset scope vars
+              $scope.vendInProgress = data.vendInProgress;
+              $scope.sessionStarted = data.sessionStarted;
+            });
+          });
+        }
     });
   };
 
   // Count down from X to 0
   function countdown(){
-  	$http.get('/status').success(function(data){
-  		console.log('ping status',data);
-  		// Continuously set vars until session is ended
-  		$scope.sessionStarted = data.sessionStarted;
-  		$scope.vendInProgress = data.vendInProgress;
+    $http.get('/status').success(function(data){
+      console.log('ping status',data);
+      // Continuously set vars until session is ended
+      $scope.sessionStarted = data.sessionStarted;
+      $scope.vendInProgress = data.vendInProgress;
       $scope.vendSuccess = data.vendSuccess;
-  		console.log('!$scope.sessionStarted',!$scope.sessionStarted);
-  		// As long as no vend is in progress, keep counting down
-  		if (!$scope.vendInProgress && !$scope.vendSuccess) {
-    		if (seconds === 0) {
-      			$scope.endSession();
-     	 		return;
-    		}
-    		$scope.time = seconds;
-    		seconds --;
-  		} else if ($scope.vendSuccess) {
-  			// TODO change conditional so that if a vend has been successful clearTimout, right now it starts counting down again and does a second end session.
-  			// If the status ping returns that the session has ended, clearTimeout
-  			console.log('clearTimeout');
-  			clearTimeout(timer);
-  		}
-  		// TODO: add case for when user is stupid and selects an empty row, then chastise them.
-  		timer = setTimeout(countdown, 1000);
-  	});
+
+      // As long as no vend is in progress, keep counting down
+      if (!$scope.vendInProgress && !$scope.vendSuccess) {
+        if (seconds === 0) {
+            $scope.endSession();
+          return;
+        }
+        $scope.time = seconds;
+        seconds --;
+      } else if ($scope.vendSuccess) {
+        // If the session was successful, clearTimeout()
+        console.log('clearTimeout');
+        clearTimeout(timer);
+      }
+      // TODO: add case for when user is stupid and selects an empty row, then chastise them.
+      timer = setTimeout(countdown, 1000);
+    });
   }
 
 
